58d164c9ecee695dae552bf1eb8191bfc1f232af
Sophien Ababssi <abasop1@gmail.com>
2022-11-20 10:36:03 +0100

    algo fizz buzz

--- algo/FizzBuzz.php
+++ algo/FizzBuzz.php
@@ -1,1 +1,20 @@
 <?php
+
+function fizzBuzz($number)
+{
+    if ($number % 15 == 0) {
+        return 'FizzBuzz';
+    }
+    if ($number % 3 == 0) {
+        return 'Fizz';
+    }
+    if ($number % 5 == 0) {
+        return 'Buzz';
+    }
+    return $number;
+}
+
+for ($i = 1; $i <= 20; $i++) {
+    echo fizzBuzz($i);
+    echo "\n";
+}
